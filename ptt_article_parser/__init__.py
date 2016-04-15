2738e205882350e97a17696758725c2ffc6aa7a8
eight <eight04@gmail.com>
2016-04-15 21:22:16 +0800

    Add more patterns

--- ptt_article_parser/__init__.py
+++ ptt_article_parser/__init__.py
@@ -10,54 +10,59 @@ ENCODING = "uao_decode"
 class NoMatch(Exception):
 	pass
 
-def search(pattern, text):
-	"""Wrap re.search. Raise NoMatch error if no match. Return decoded text."""
-	match = re.search(pattern, text, re.MULTILINE)
-	if not match:
-		raise NoMatch
-	return [x.decode(ENCODING) for x in match.groups()]
-
 class Article:
 	def __init__(self, source):
 		"""Give source Bytes to build an article"""
 		self.originalSource = source
 		self.source = re.sub(br"\x1b\[[\d;]*m", br"", source)
+	
+	def search(self, *patterns):
+		for pattern in patterns:
+			match = re.search(pattern, self.source, re.MULTILINE)
+			if match:
+				return match.group(1).decode(ENCODING)
+		return None
 
 	def getTitle(self):
 		"""Get the title"""
 		if not hasattr(self, "title"):
-			try:
-				self.title, = search(br"\xbc\xd0\xc3D[: ]\s?(.+?)\s*$", self.source)
-			except NoMatch:
-				self.title = None
+			self.title = self.search(
+				br"\xbc\xd0\xc3D[: ]\s?(.+?)\s*$"
+			)
 		return self.title
 
 	def getAuthor(self):
 		"""Get the author"""
 		if not hasattr(self, "author"):
-			try:
-				self.author, = search(br"\xa7@\xaa\xcc:?\s*?(.+?)\s*?(?:\xac\xdd\xaaO|\xaf\xb8\xa4\xba|$)", self.source)
-			except NoMatch:
-				self.author = None
+			self.author = self.search(
+				br"\xa7@\xaa\xcc:?\s*?(.+?)\s*?(?:\xac\xdd\xaaO|\xaf\xb8\xa4\xba|$)",
+				br"\xa1\xb0 \xbds\xbf\xe8:\s*(\w+)"
+			)
 		return self.author
 
 	def getTime(self):
 		"""Get post time"""
 		if not hasattr(self, "time"):
-			try:
-				self.time, = search(br"\xae\xc9\xb6\xa1[: ]\s?(\w+ \w+  ?\d+ \d+:\d+:\d+ \d+)", self.source)
-				self.time = datetime.datetime.strptime(self.time, "%a %b %d %H:%M:%S %Y")
-			except NoMatch:
-				self.time = None
+			for pattern, format in ((
+						br"\xae\xc9\xb6\xa1[: ]\s?(\w+ \w+  ?\d+ \d+:\d+:\d+ \d+)",
+						"%a %b %d %H:%M:%S %Y"
+					),(
+						b'\xa1\xb0 \xbds\xbf\xe8:.+?, (\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2})',
+						"%m/%d/%Y %H:%M:%S"
+					)):
+				self.time = self.search(pattern)
+				if self.time:
+					self.time = datetime.datetime.strptime(self.time, format)
+					break
 		return self.time
 
 	def getBoard(self):
 		"""Get board name"""
 		if not hasattr(self, "board"):
-			try:
-				self.board, = search(br"(?:\xac\xdd\xaaO|\xaf\xb8\xa4\xba)[: ]\s?(\S+)", self.source)
-			except NoMatch:
-				self.board = None
+			self.board = self.search(
+				br"(?:\xac\xdd\xaaO|\xaf\xb8\xa4\xba)[: ]\s?(\S+)",
+				b'\xa1\xb0 \xa4\xe5\xb3\xb9\xba\xf4\xa7}: https://www\\.ptt\\.cc/bbs/([^/]+)'
+			)
 		return self.board
 
 	def getBody(self):
