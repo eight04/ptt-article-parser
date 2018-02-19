9faba3dd2e6fe988669ae5a280e2d1d490b6cdf9
eight <eight04@gmail.com>
2018-02-19 15:12:55 +0800

    fixup! Regenerate pylintrc

--- ptt_article_parser/__init__.py
+++ ptt_article_parser/__init__.py
@@ -16,30 +16,30 @@ class Article:
 	def __init__(self, source):
 		"""Give source Bytes to build an article"""
 		self.original_source = source
-		
+
 		# remove ansi, encode to string
 		self.source = strip_color(source).decode(ENCODING)
-		
+
 		# get headers
 		matches = re.finditer(r'作者:?\s*(.+?) *(?:看板:?\s*([a-zA-Z0-9-_]+) *)?\n\s*標題:?\s*(.+?) *\n\s*時間:?\s*(\w+ \w+  ?\d+ \d+:\d+:\d+ \d+) *\n?(?:─+ *\r?\n?)?', self.source)
 		self.headers = [Header(*m.groups(), m.start, m.end) for m in matches]
-		
+
 		# get forward heads
 		matches = re.finditer(r"※ \[本文轉錄自\s*([a-zA-Z0-9-_]+)\s*看板\s*(#[a-zA-Z0-9-_]{8})\s*\] *\n?", self.source)
 		self.forward_heads = [ForwardHead(*m.groups(), m.start(), m.end()) for m in matches]
-		
+
 		# get forward foot
 		matches = re.finditer(r"※ 發信站: 批踢踢實業坊\(ptt\.cc\)\s*※ 轉錄者: (\w+) \((\d+.\d+.\d+.\d+)\),\s*(?:時間:\s*)?(\d+/\d+/\d+ \d+:\d+:\d+) *\n?", self.source)
 		self.forward_foots = [ForwardFoot(*m.groups(), m.start(), m.end()) for m in matches]
-				
+
 		# get sign
 		match = re.search(r"※ 發信站: 批踢踢實業坊\(ptt\.cc\), 來自: (\d+\.\d+\.\d+\.\d+) *\n※ 文章網址: (http\S+) *\n?", self.source)
 		self.sign = match and Sign(*match.groups(), match.start(), match.end())
-			
+
 		# old sign
 		match = re.search(r"※ 發信站: 批踢踢實業坊\(ptt\.cc\)\n◆ From: (\d+\.\d+\.\d+\.\d+)", self.source)
 		self.old_sign = match and Sign(match.group(1), None, match.start(), match.end())
-				
+
 		# get edits
 		self.edits = []
 		matches = re.finditer(r"※ 編輯: (\w+) \((\d+\.\d+\.\d+\.\d+)\), (\d+/\d+/\d+ \d+:\d+:\d+)", self.source)
@@ -47,7 +47,7 @@ class Article:
 			author, ip, time = match.groups()
 			time = datetime.datetime.strptime(time, "%m/%d/%Y %H:%M:%S")
 			self.edits.append(Edit(author, ip, time, match.start(), match.end()))
-			
+
 		# old edits
 		self.old_edits = []
 		matches = re.finditer(r"※ 編輯: (\w+)\s*來自: (\d+\.\d+\.\d+\.\d+)\s*\((\d+/\d+ \d+:\d+)\)", self.source)
@@ -55,7 +55,7 @@ class Article:
 			author, ip, time = match.groups()
 			time = datetime.datetime.strptime(time, "%m/%d %H:%M")
 			self.old_edits.append(Edit(author, ip, time, match.start(), match.end()))
-	
+
 	def getTitle(self):
 		"""Get the title"""
 		i = len(self.forward_heads)
@@ -124,7 +124,7 @@ class Article:
 	def getPushes(self):
 		"""Get push"""
 		pass
-		
+
 class ForwardHead:
 	"""Forwarding info header"""
 	def __init__(self, board, aid, start, end):
@@ -132,7 +132,7 @@ class ForwardHead:
 		self.aid = aid
 		self.start = start
 		self.end = end
-		
+
 class Header:
 	"""Article header"""
 	def __init__(self, author, board, title, time, start, end):
