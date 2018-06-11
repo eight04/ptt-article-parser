0f734f20d9c6ae97f88612494d59d1f0a69af8f4
eight <eight04@gmail.com>
2018-06-12 01:58:52 +0800

    Change: tools -> rename

--- ptt_article_parser/__main__.py
+++ ptt_article_parser/__main__.py
@@ -7,26 +7,26 @@ Usage:
   pap rename [--format=<format>] [--dir=<file>] <file>...
   pap rename [--format=<format>] [--dir=<file>] --interactive
   pap (--help | --version)
-	
+
 Options:
   -v --version          Show version.
   -h --help             Show this.
-  -f --format=<format>  Set output format. 
+  -f --format=<format>  Set output format.
                         [default: [{board}] {title} [{author}] ({time:%Y%m%d%H%M%S}).ans]
-  -d --dir=<file>       Read additional ".DIR" file. The tool always tries to 
+  -d --dir=<file>       Read additional ".DIR" file. The tool always tries to
                         read the ".DIR" file under the parent folder of the
                         article. Use this option to read from other locations.
   -i --interactive      Use interactive mode, get file name from stdin.
   <file>                File path. If the file doesn't exists, pap will try to
                         parse it as glob pattern.
-  
+
 """
 
 import glob
 import os.path
 import docopt
 from . import __version__
-from .tools import rename
+from .rename import rename
 
 def do_rename(pattern, format, dir=None):
 	"""Use glob pattern if file dosn't exist"""
@@ -42,7 +42,7 @@ def do_rename(pattern, format, dir=None):
 def main():
 	"""Main entry"""
 	args = docopt.docopt(__doc__, version=__version__)
-	
+
 	# Rename file
 	if args["rename"]:
 		def files_from_input():
@@ -54,14 +54,14 @@ def main():
 						yield file
 				except EOFError:
 					break
-					
+
 		from .dir import DIR
 		dir = DIR()
 		if args["--dir"]:
 			dir.read_file(args["--dir"])
-			
+
 		for file in files_from_input() if args["--interactive"] else args["<file>"]:
 			do_rename(file, args["--format"], dir=dir)
-				
+
 if __name__ == "__main__":
 	main()
--- ptt_article_parser/rename.py
+++ ptt_article_parser/rename.py
@@ -8,31 +8,39 @@ from . import Article
 from .helper import safe_file_name, format_dummy
 from .dir import DIR
 
+def format_filename(article, file=None, dir=None, format=None, extra=None):
+	"""Get formatted filename."""
+	context = {
+		"title": dir and dir.getTitle(file) or article.getTitle(),
+		"author": article.getAuthor() or dir.getAuthor(file),
+		"board": article.getBoard(),
+		"time": article.getTime() or dir.getTime(file) or format_dummy
+	}
+	if extra:
+		context.update(extra)
+	return safe_file_name(format.format_map(context))
+
 def rename(file, format_spec, dir=DIR()):
 	"""Rename article with specified format"""
 	file = pathlib.Path(file)
-	
+
 	print("Parsing {name}...".format(name=file.name))
-	
 	article = Article(file.read_bytes())
-	
-	new_file = format_spec.format(
-		article = article,
-		title = dir.getTitle(file) or article.getTitle(),
-		author = article.getAuthor() or dir.getAuthor(file),
-		board = article.getBoard(),
-		time = article.getTime() or dir.getTime(file) or format_dummy
-	)
-	new_file = safe_file_name(new_file)
-	new_file = file.with_name(new_file)
-	
+
+	new_file = file.with_name(format_filename(
+		article,
+		file=file,
+		dir=dir,
+		format=format_spec
+	))
+
 	if file == new_file:
 		print("Same file name!\n")
 		return
-	
+
 	if new_file.exists():
 		num = 2
-		
+
 		while True:
 			temp_file = "{name} ({num}){ext}".format(
 				num = num,
@@ -40,17 +48,17 @@ def rename(file, format_spec, dir=DIR()):
 				ext = new_file.suffix
 			)
 			temp_file = new_file.with_name(temp_file)
-			
+
 			if file == temp_file:
 				print("Same file name!\n")
 				return
-				
+
 			if not temp_file.exists():
 				new_file = temp_file
 				break
-				
+
 			num += 1
-	
+
 	print("Rename to {name}...\n".format(name=new_file.name))
-	
+
 	file.rename(new_file)
