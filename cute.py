a865693f5a8b96460f523d305ff277356548ab51
eight <eight04@gmail.com>
2018-06-12 02:38:01 +0800

    Update dependencies

--- cute.py
+++ cute.py
@@ -1,13 +1,20 @@
 #! python3
 
-from xcute import cute, exc
+from xcute import cute
+
+def readme():
+    """Live reload readme"""
+    from livereload import Server
+    server = Server()
+    server.watch("README.rst", "py cute.py readme_build")
+    server.serve(open_url_delay=1, root="build/readme")
 
 cute(
 	pkg_name = "ptt_article_parser",
 	test = ["pylint {pkg_name}", 'readme_build'],
 	bump_pre = 'test',
 	bump_post = ['dist', 'release', 'publish', 'install'],
-	dist = 'rm -r build dist & python setup.py sdist bdist_wheel',
+	dist = 'x-clean build dist && python setup.py sdist bdist_wheel',
 	release = [
 		'git add .',
 		'git commit -m "Release v{version}"',
@@ -17,16 +24,12 @@ cute(
 		'twine upload dist/*',
 		'git push --follow-tags'
 	],
-	publish_err = 'start https://pypi.python.org/pypi/ptt-article-parser/',
 	install = 'pip install -e .',
-	install_err = 'elevate -c -w pip install -e .',
 	readme_build = [
-		'python setup.py --long-description | x-pipe build/ld.rst',
-		'rst2html --no-raw --exit-status=1 --verbose '
-			'build/ld.rst build/ld.html'
-	],
-	readme_build_err = ['readme_show', exc],
-	readme_show = 'start build/ld.html',
-	readme = 'readme_build',
-	readme_post = 'readme_show'
+        'python setup.py --long-description | x-pipe build/readme/index.rst',
+        ('rst2html5.py --no-raw --exit-status=1 --verbose '
+         'build/readme/index.rst build/readme/index.html')
+    ],
+    readme_pre = "readme_build",
+    readme = readme
 )
